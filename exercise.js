9f4c99ae808cb39428da99ae3d9501ab52cc3f89
orearow <oreo.crumbs@gmail.com>
2019-10-03 15:52:49 -0600

    cleaning up and fixing code, adding some comments

--- exercise.js
+++ exercise.js
@@ -1,6 +1,6 @@
 function time(array){
 
-
+	//itterating through array and turning our times into 4 digits
 	for(let elem in array){
 		array[elem] = array[elem].replace(":", "");
 	}
@@ -10,28 +10,28 @@ function time(array){
 	let distance = "";
 	let newDistance = "";
 	let pair = "";
-	console.log(sortedArray)
 
 	for( let i=0; i<= sortedArray.length -1; i++){
+		//on first itteration we set distance as the distance between first two elems
 		if( i === 0){
 			distance = sortedArray[i +1 ] - sortedArray[i];
+			// putting the colon back in and turning digit into time
+			tostring1 = sortedArray[i +1 ].slice(0,2)+ ":"+ sortedArray[i+1].slice(2,4);
+			tostring2 = sortedArray[i].slice(0,2)+ ":"+ sortedArray[i].slice(2,4);
 
-			tostring1 = sortedArray[i +1 ].slice(0,2)+ ":"+ sortedArray[i+1].slice(2,4)
-			tostring2 = sortedArray[i].slice(0,2)+ ":"+ sortedArray[i].slice(2,4)
-
-			pair = tostring1 + " "+ tostring2
+			pair = tostring1 + " "+ tostring2;
 		}
+		//next itterations
 		else{
-			console.log(newDistance)
-			console.log(distance)
-			newDistance = sortedArray[i +1 ] - sortedArray[i];
 
+			newDistance = sortedArray[i +1 ] - sortedArray[i];
+			//comparing old and new distance values and outputting the smallest one 
 			if(newDistance < distance){
 				distance = newDistance;
-				tostring1 = sortedArray[i +1 ].slice(0,2)+ ":"+ sortedArray[i+1].slice(2,4)
-				tostring2 = sortedArray[i ].slice(0,2)+ ":"+ sortedArray[i].slice(2,4)
+				tostring1 = sortedArray[i +1 ].slice(0,2)+ ":"+ sortedArray[i+1].slice(2,4);
+				tostring2 = sortedArray[i ].slice(0,2)+ ":"+ sortedArray[i].slice(2,4);
 
-				pair = tostring1 + " " + tostring2
+				pair = tostring1 + " " + tostring2;
 
 			}
 		}
@@ -41,3 +41,4 @@ function time(array){
 	return pair;
 
 }
+console.log(time(['04:53', '12:15', '03:14', '22:18', '16:33', '21:59']));
