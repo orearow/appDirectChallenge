d49dff862164e93a48e4b9d7b32e4777eff52977
orearow <oreo.crumbs@gmail.com>
2019-10-03 16:34:51 -0600

    edgecase,nums are close but one is after midnight

--- exercise.js
+++ exercise.js
@@ -29,16 +29,26 @@ function time(array){
 			if(newDistance < distance){
 				distance = newDistance;
 				tostring1 = sortedArray[i +1 ].slice(0,2)+ ":"+ sortedArray[i+1].slice(2,4);
-				tostring2 = sortedArray[i ].slice(0,2)+ ":"+ sortedArray[i].slice(2,4);
+				tostring2 = sortedArray[i].slice(0,2)+ ":"+ sortedArray[i].slice(2,4);
 
 				pair = tostring1 + " " + tostring2;
 
 			}
 		}
-
-
 	}
+
+		//edge case if we have one value like 0001 and one like 2359 for ex, lets just se 23:60 as 2400 since time does not work like math 
+		if(sortedArray[0] < '1200' && sortedArray[sortedArray.length-1] > '1200') {
+			let edgeDiff = '2360' -(sortedArray[sortedArray.length-1] -sortedArray[0]);
+			if (edgeDiff< distance){
+				tostring1 = sortedArray[0].slice(0,2)+ ":"+ sortedArray[0].slice(2,4);
+				tostring2 = sortedArray[sortedArray.length-1].slice(0,2)+ ":"+ sortedArray[sortedArray.length-1].slice(2,4);
+				return tostring1 + " " + tostring2;
+			}
+		}
+
 	return pair;
 
 }
-console.log(time(['04:53', '12:15', '03:14', '22:18', '16:33', '21:59']));
+//sample data I was using 
+// console.log(time(['04:53', '12:15','00:01','22:00' ,'03:14','07:59' ,'22:18', '16:33','23:59' ,'21:59']));
